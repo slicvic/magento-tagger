7c429df0fe4f074670fb8ed00efc95985d49d0f3
Victor Lantigua <victor@Victors-MacBook-Pro.local>
2016-12-24 16:09:06 -0500

    Add model exception classes

---
+++ src/app/code/local/Wfn/Tagger/Model/Exception.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Model exception.
+ */
+class Wfn_Tagger_Model_Exception extends Exception
+{
+        //
+}
---
+++ src/app/code/local/Wfn/Tagger/Model/Exception/Validation.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Model validation exception.
+ */
+class Wfn_Tagger_Model_Exception_Validation extends Wfn_Tagger_Model_Exception
+{
+        //
+}
