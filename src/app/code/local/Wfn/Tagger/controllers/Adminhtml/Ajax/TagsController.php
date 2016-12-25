63632181f19f938ac3eaac93613d86e499348a04
Victor Lantigua <victor@Victors-MacBook-Pro.local>
2016-12-25 11:48:33 -0500

    Implement add tag controller action

---
+++ src/app/code/local/Wfn/Tagger/controllers/Adminhtml/Ajax/TagsController.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Tag controller.
+ */
+class Wfn_Tagger_Adminhtml_Ajax_TagsController extends Mage_Adminhtml_Controller_Action
+{
+    /**
+     * Proccess request to tag an entity.
+     *
+     * @return JSON string
+     */
+    public function addTagAction()
+    {
+        $request = $this->getRequest();
+        $params = $request->getParams();
+        $response = [
+            'success' => false
+        ];
+
+        if (
+            empty($params['tag']['name'])
+            || empty($params['tag']['assigned_entity_id'])
+            || empty($params['tag']['assigned_entity_type'])
+        ) {
+            $response['error_message'] = $this->__('Invalid tag data.');
+            return $this->sendJsonResponse($response);
+        }
+
+        try {
+            Wfn_Tagger_Model_Tag::createTagAndAssignEntity(
+                $params['tag']['name'],
+                $params['tag']['assigned_entity_id'],
+                $params['tag']['assigned_entity_type'],
+                Mage::getSingleton('admin/session')->getUser()->getUserId()
+            );
+
+
+            $response['success'] = true;
+
+        } catch (Wfn_Tagger_Model_Validation_Exception $e) {
+            $response['error_message'] = $this->__($e->getMessage());
+        } catch (Exception $e) {
+            Mage::log($e->getMessage());
+            $response['error_message'] = $this->__('Whoops! Something went wrong. Please try again!');
+        }
+
+        return $this->sendJsonResponse($response);
+    }
+
+    /**
+     * Proccess request to untag an entity.
+     */
+    public function removeTagAction()
+    {
+        //
+    }
+
+    /**
+     * Send a JSON response.
+     *
+     * @param  array  $data
+     */
+    private function sendJsonResponse(array $data)
+    {
+        $this->getResponse()->setHeader('Content-type', 'application/json');
+        $this->getResponse()->setBody(json_encode($data));
+    }
+}
