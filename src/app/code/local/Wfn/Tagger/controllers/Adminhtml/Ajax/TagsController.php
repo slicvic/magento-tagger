7579a4d9db9af27f353d31371383309254a0c4c7
Victor Lantigua <victor@Victors-MacBook-Pro.local>
2016-12-26 18:56:30 -0500

    Move js from template to main.js

--- src/app/code/local/Wfn/Tagger/controllers/Adminhtml/Ajax/TagsController.php
+++ src/app/code/local/Wfn/Tagger/controllers/Adminhtml/Ajax/TagsController.php
@@ -16,19 +16,19 @@ class Wfn_Tagger_Adminhtml_Ajax_TagsController extends Mage_Adminhtml_Controller
         $response = ['success' => false];
 
         if (
-            empty($params['tag']['name'])
-            || empty($params['tag']['assigned_entity_id'])
-            || empty($params['tag']['assigned_entity_type'])
+            empty($params['tag_name'])
+            || empty($params['entity_id'])
+            || empty($params['entity_type'])
         ) {
-            $response['error_message'] = $this->__('Invalid tag parameter.');
+            $response['error_message'] = $this->__('Invalid parameters.');
             return $this->sendJsonResponse($response);
         }
 
         try {
             Wfn_Tagger_Model_Resource_Tag::createTagAndAssignEntity(
-                $params['tag']['name'],
-                $params['tag']['assigned_entity_id'],
-                $params['tag']['assigned_entity_type'],
+                $params['tag_name'],
+                $params['entity_id'],
+                $params['entity_type'],
                 Mage::getSingleton('admin/session')->getUser()->getUserId()
             );
 
@@ -55,16 +55,16 @@ class Wfn_Tagger_Adminhtml_Ajax_TagsController extends Mage_Adminhtml_Controller
         $response = ['success' => false];
 
         if (
-            empty($params['tag']['name'])
-            || empty($params['tag']['assigned_entity_id'])
-            || empty($params['tag']['assigned_entity_type'])
+            empty($params['tag_name'])
+            || empty($params['entity_id'])
+            || empty($params['entity_type'])
         ) {
-            $response['error_message'] = $this->__('Invalid tag parameter.');
+            $response['error_message'] = $this->__('Invalid parameters.');
             return $this->sendJsonResponse($response);
         }
 
         try {
-            $tag = Wfn_Tagger_Model_Resource_Tag::loadByName($params['tag']['name']);
+            $tag = Wfn_Tagger_Model_Resource_Tag::loadByName($params['tag_name']);
             if (!$tag->getId()) {
                 $response['error_message'] = $this->__('Tag not found.');
                 return $this->sendJsonResponse($response);
@@ -72,8 +72,8 @@ class Wfn_Tagger_Adminhtml_Ajax_TagsController extends Mage_Adminhtml_Controller
 
             Wfn_Tagger_Model_Resource_TagRelation::deleteByTagIdEntityIdAndEntityType(
                 $tag->getId(),
-                $params['tag']['assigned_entity_id'],
-                $params['tag']['assigned_entity_type']
+                $params['entity_id'],
+                $params['entity_type']
             );
 
             $response['success'] = true;
---
+++ src/js/wfn_tagger/main.js
@@ -0,0 +1,133 @@
+var WfnTaggerModule = (function($) {
+    var settings = {
+        formKey: null,
+        tagUrl: null,
+        untagUrl: null,
+        entityId: null,
+        entityType: null
+    };
+    var elements = {
+        loadingMask: null,
+        tagInput: null,
+        tagButtons: null
+    };
+    var isProcessing = false;
+    var selectize;
+
+    function init(_settings) {
+        settings = _settings || {};
+        bindElements();
+        bindSelectize();
+        bindTagButtons();
+    }
+
+    function bindElements() {
+        elements.loadingMask = $('#loading-mask');
+        elements.tagInput = $('#tag-input');
+        elements.tagButtons = $('#tag-buttons');
+    }
+
+    function bindTagButtons() {
+        elements.tagButtons.on('click', 'button', function() {
+            var tagName = $(this).data('name');
+            selectize.createItem(tagName);
+        });
+    }
+
+    function bindSelectize() {
+        elements.tagInput.selectize({
+            plugins: ['remove_button'],
+            delimiter: ',',
+            persist: false,
+            valueField: 'id',
+            labelField: 'name',
+            searchField: 'name',
+            create: function(input, callback) {
+                if (selectize.getItem(input).length) {
+                    alert('Tag already added!');
+                    return callback(false);
+                }
+
+                if (isProcessing) {
+                    return callback(false);
+                }
+
+                isProcessing = true;
+                elements.loadingMask.show();
+
+                $.ajax({
+                    url: settings.tagUrl,
+                    method: 'post',
+                    dataType: 'json',
+                    data: {
+                        tag_name: input,
+                        form_key: settings.formKey,
+                        entity_id: settings.entityId,
+                        entity_type: settings.entityType
+                    }
+                }).done(function(response) {
+                    if (response.success) {
+                        callback({
+                            id: input,
+                            name: input
+                        });
+                    } else {
+                        callback(false);
+                        alert(response.error_message);
+                    }
+                }).fail(function() {
+                    callback(false);
+                    alert('Oops! Something went wrong. Please try again!');
+                }).always(function() {
+                    isProcessing = false;
+                    elements.loadingMask.hide();
+                });
+            },
+            onDelete: function(values) {
+                if (!values || values.length !== 1) {
+                    return false;
+                }
+
+                if (isProcessing) {
+                    return false;
+                }
+
+                var value = values[0];
+                isProcessing = true;
+                elements.loadingMask.show();
+
+                $.ajax({
+                    url: settings.untagUrl,
+                    method: 'post',
+                    dataType: 'json',
+                    data: {
+                        tag_name: value,
+                        form_key: settings.formKey,
+                        entity_id: settings.entityId,
+                        entity_type: settings.entityType
+                    }
+                }).done(function(response) {
+                    if (response.success) {
+                        selectize.removeItem(value);
+                        selectize.removeOption(value);
+                    } else {
+                        alert(response.error_message);
+                    }
+                }).fail(function() {
+                    alert('Oops! Something went wrong. Please try again!');
+                }).always(function() {
+                    isProcessing = false;
+                    elements.loadingMask.hide();
+                });
+
+                return false;
+            }
+        });
+
+        selectize = elements.tagInput[0].selectize;
+    }
+
+    return {
+        init: init
+    };
+}(jQuery));
