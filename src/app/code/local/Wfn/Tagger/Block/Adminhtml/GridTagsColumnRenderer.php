82588616fc75ad646fd33fd73a9f6ee9f43436e1
victor <dot@mymngmt.com>
2017-01-23 09:10:21 -0500

    Change tag badges to div

--- src/app/code/local/Wfn/Tagger/Block/Adminhtml/GridTagsColumnRenderer.php
+++ src/app/code/local/Wfn/Tagger/Block/Adminhtml/GridTagsColumnRenderer.php
@@ -13,7 +13,7 @@ class Wfn_Tagger_Block_Adminhtml_GridTagsColumnRenderer extends Mage_Adminhtml_B
         if (empty($value)) {
             return '';
         }
-        $style = 'background-color:#7f8c8d;color:#fff;padding:3px;margin:2px;border-radius:3px;';
-        return "<div style=\"margin-top:4px\"><span style=\"$style\">" . str_replace(',', "</span><span style=\"$style\">", $value) . "</span></div>";
+        $style = 'background-color:#7f8c8d;color:#fff;padding:3px;margin:2px;border-radius:3px;display:inline-block;';
+        return "<div style=\"$style\">" . str_replace(',', "</div><div style=\"$style\">", $value) . "</div>";
     }
 }
